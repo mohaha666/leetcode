4d83c006036b48b718a4d14106a84db197c67e6b
mohaha666 <jiamo88621@163.com>
2021-09-29 16:27:10 +0800

    Update 反转链表.java

--- 反转链表.java
+++ 反转链表.java
@@ -1,3 +1,19 @@
+
+/**
+解题思路
+此处撰写解题思路
+
+pre是新的尾巴，cur是当前的元素，tmp是cur下一个元素
+如果cur或tmp为null，那么直接返回cur就可以，说明这个链没有元素或就一个元素
+pre是尾巴，cur指向head头，tmp指向cur的下一个元素
+先将cur的next指向pre，即断链
+然后，所有指针向后移动，pre指向cur，cur指向tmp，tmp指向tmp的next
+直到cur的next为null时，就到了最顶端了，这时退出循环
+然后将cur的next指向pre，这时最后一个元素也链了上去，这时返回cur即可
+
+*/
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
